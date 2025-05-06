d9233b146188d4466d447d063425c18c9d420667
unknown <wilfriedngassarou23@gmail.com>
2025-05-06 23:37:34 +0200

    Update slapsh screen background opacity

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -1,6 +1,6 @@
 export function SplashScreen() {
   return (
-    <section className="fixed inset-0 z-30 bg-black/90 flex justify-center items-center">
+    <section className="fixed inset-0 z-30 bg-black/50 flex justify-center items-center">
       <div className="bg-white rounded-lg w-80 h-40 flex flex-col gap-4 justify-center items-center">
         <h1 className="font-medium text-lg text-center">
           Initialisation du dictionnaire...
