8cc48bc5b2c1cb39ff45be7b7ec0b0cbf589728c
unknown <wilfriedngassarou23@gmail.com>
2025-05-06 18:31:58 +0200

    add svgs

---
+++ src/components/svgs/arrow-up.tsx
@@ -0,0 +1,13 @@
+export function ArrowUp() {
+  return (
+    <svg
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="w-6 h-6 fill-none stroke-white"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+    >
+      <path d="m5 12 7-7 7 7M12 19V5" />
+    </svg>
+  )
+}
---
+++ src/components/svgs/mic.tsx
@@ -0,0 +1,14 @@
+export function Mic() {
+  return (
+    <svg
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="w-6 h-6 fill-none stroke-gray-700"
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3" />
+      <path d="M19 10v2a7 7 0 0 1-14 0v-2M12 19v3" />
+    </svg>
+  )
+}
